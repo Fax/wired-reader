ff4b2750c12f227071812e8a233c6719c5722657
Fabrizio Rapelli <fabrizio.rapelli@dovetail.ie>
2017-03-07 10:01:32 +0000

    multiple urls used as index. requests resolved with promises

--- server.js
+++ server.js
@@ -12,48 +12,73 @@ app.get('/read/:wiredUrl', function (req, res) {
   request(url, function (error, response, html) {
     if (!error) {
       var $ = cheerio.load(html);
-      
+
 
       if ($('body').hasClass('feature-story-layout')) {
         // other scraper
-        
+
         var sections = $('section').map((index, section) => {
           dd.push($(section).html());
         });
         article = dd.join('');
-        
+
       } else {
 
         article = $('article').html();
-      }  
+      }
     }
     res.send(article)
   })
 })
 
 app.get('/', function (req, res) {
-  var url = "https://www.wired.com/";
+
+  var urls = [
+    'https://www.wired.com/',
+    'https://www.wired.com/category/business/',
+    'https://www.wired.com/category/culture/',
+    'https://www.wired.com/category/design/',
+    'https://www.wired.com/category/gear/',
+    'https://www.wired.com/category/science/',
+    'https://www.wired.com/category/security/',
+    'https://www.wired.com/category/transportation/',
+  ]
+
   var article = "";
-  var dd = [];
-  request(url, function (error, response, html) {
-    if (!error) {
-      var $ = cheerio.load(html);
-
-      
-      var cards = $('[itemtype="http://schema.org/Article"]');//.find('a').attr('href');
-      cards.map(function (index, el) {
-        var addr = $(el).find('a').attr('href');
-
-        dd.push(
-          "<li><a href='/read/" +
-          encodeURIComponent(addr)+ "'>"+addr+"</a></li>"
-
-        );
+  var promises = [];
+  for (var i = 0; i < urls.length; i++) {
+    var url = urls[i];
+    var p = new Promise(function (fulfill, reject) {
+      request(url, function (error, response, html) {
+        if (!error) {
+          var dd = [];
+          var $ = cheerio.load(html);
+          var cards = $('[itemtype="http://schema.org/Article"]');
+          cards.map(function (index, el) {
+            var addr = $(el).find('a').attr('href');
+            dd.push(
+              "<li><a href='/read/" + encodeURIComponent(addr) + "'>" + addr + "</a></li>"
+            );
+          })
+          fulfill(dd);
+        } else {
+          reject(err);
+        }
       })
-      article = "<ul>" + dd.join('') + "</ul>";
+    });
+    promises.push(p)
+  }
+  Promise.all(promises).then(
+    function (dd) {
+      for (var i = 0; i < dd.length; i++){
+      var listItems = dd[i].filter(function (elem, pos) {
+        return dd[i].indexOf(elem) == pos;
+      })
+      article += "<ul>" + listItems.join('') + "</ul>";
+      }
+      res.send(article)
     }
-    res.send(article)
-  })
+  )
 })
 
 app.listen(serverPort);
